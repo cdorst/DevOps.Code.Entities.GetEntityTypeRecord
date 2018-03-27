d356c9a33f3e598156ea3ffc942c13fb4dd82a9b
Christopher Dorst <chris@dorst.email>
2018-03-26 20:04:21 -0500

    Add EntityTypeRecordGetter.cs

---
+++ DevOps.Code.Entities.GetEntityTypeRecord/EntityTypeRecordGetter.cs
@@ -0,0 +1,24 @@
+// Copyright © Christopher Dorst. All rights reserved.
+// Licensed under the GNU General Public License, Version 3.0. See the LICENSE document in the repository root for license information.
+
+using DevOps.Code.Entities.EntityTypeLedger;
+using Microsoft.WindowsAzure.Storage.Table;
+using System.Threading.Tasks;
+using static DevOps.Code.Entities.GetAzureTable.AzureTableGetter;
+
+namespace DevOps.Code.Entities.GetEntityTypeRecord
+{
+    /// <summary>Function gets the given entity-type's ID record from the Azure Storage Table entity-types ledger</summary>
+    public static class EntityTypeRecordGetter
+    {
+        /// <summary>Returns the given entity-type's ID record from the Azure Storage Table entity-types ledger</summary>
+        public static async Task<RepositoryVersionTable> GetEntityTypeRecordAsync(string accountName, string repositoryName)
+        {
+            var operation = TableOperation.Retrieve<EntityTypeTable>(accountName, repositoryName);
+            var table = await GetTable();
+            var result = await table.ExecuteAsync(operation);
+            if (result?.Result == null) return null;
+            return (EntityTypeTable)result.Result;;
+        }
+    }
+}
